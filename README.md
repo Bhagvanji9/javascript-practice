01031240965a0db626149f8deffc1fe3ea07aff0
bhagvanji nakum <bhagvanji@simformsolutions.com>
2023-03-20 17:41:59 +0530

    Covered basics of javascript object.

--- README.md
+++ README.md
@@ -16,3 +16,5 @@
 ### Variables
 
 ### Arrays
+
+### Objects
---
+++ js-object/object.js
@@ -0,0 +1,58 @@
+// const person = {
+//   name: ["Bob", "Smith"],
+//   age: 32,
+//   bio: function () {
+//     console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+//   },
+//   introduceSelf: function () {
+//     console.log(`Hi! I'm ${this.name[0]}.`);
+//   },
+// };
+// person.bio();
+// person.introduceSelf();
+// console.log(person.name);
+// console.log(person.name[0]);
+// console.log(person.age);
+
+// // An object property can itself be an object
+// const person1 = {
+//   name: ["Bob", "Smith"],
+// };
+// //is equal to
+
+// const person2 = {
+//   fullname: {
+//     first: Bob,
+//     last: smith,
+//   },
+// };
+// // To access these items like this
+// person2.fullname.first;
+// person2.fullname.last;
+
+// const key = "name";
+// const getKey = () => "name";
+// const Obj = { name: "Michel" };
+
+// Obj["name"]; // returns "Michel"
+// Obj[key]; // evaluates to Obj["name"], and returns "Michel"
+// Obj[getKey()]; // evaluates to Obj["name"], and returns "Michel"
+
+// const user = {
+//   fName: "bhagvanji",
+//   getName: function () {
+//     console.log(this.fName);
+//   },
+// };
+// console.log(user.getName());
+
+// example of Comparing objects
+const fName1 = {
+  first: "bhagvanji",
+};
+const fName2 = {
+  first: "bhagvanji",
+};
+const fName3 = fName1;
+console.log(fName1 == fName2);
+console.log(fName1 == fName3);
